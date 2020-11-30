b76ab525c33f9c21aa71acb3c15de34cb9b29469
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-11-30 19:07:14 -0300

    feat: Holding the player's score
    I made this change to compute and display the earned scores. Also, when
    the player hits 15 points, we got a winner.

--- script.js
+++ script.js
@@ -17,8 +17,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPLayer = function(){
+	currentScore = 0;	
+	document.getElementById(`current--${activePlayer}`).textContent = 0;
+	activePlayer = activePlayer === 0 ? 1 : 0;
+	player0El.classList.toggle('player--active');	
+	player1El.classList.toggle('player--active');
+}
 
 btnRoll.addEventListener('click', function(){
 	const dice = 1 + Math.trunc(Math.random() * 6);
@@ -28,10 +37,18 @@ btnRoll.addEventListener('click', function(){
 		currentScore += dice;	
 		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 	}else{
-		currentScore = 0;	
-		document.getElementById(`current--${activePlayer}`).textContent = 0;
-		activePlayer = activePlayer === 0 ? 1 : 0;
-		player0El.classList.toggle('player--active');	
-		player1El.classList.toggle('player--active');	
+		switchPLayer();
 	}
+});
+
+btnHold.addEventListener('click', function(){
+	scores[activePlayer] += currentScore;
+	document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+		if(scores[activePlayer] >= 15){
+			document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+			document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+		}else{
+			switchPLayer();
+		}
 });
